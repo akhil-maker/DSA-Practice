597348ac4779773f092cefe5ef2aa1109fda4064
Akhil Soni <akhil0435@gmail.com>
2022-04-12 22:58:25 +0530

    Add bit majic: feat

---
+++ Bit manipulation/notTwice.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int unique(int a[], int n){
+    int xorsum = 0;
+    for(int i=0; i<n; i++)
+        xorsum = xorsum^a[i];
+    return xorsum;
+}
+int main(){
+    int a[] = {1, 2, 3, 4, 1, 2, 3};
+    cout<<unique(a, 7);
+}
