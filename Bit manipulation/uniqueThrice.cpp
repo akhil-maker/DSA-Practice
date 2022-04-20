b1df0749dc7019e79ea8cdd1c2a49e2524c4e5e6
Akhil Soni <akhil0435@gmail.com>
2022-04-20 19:16:52 +0530

    add bit

---
+++ Bit manipulation/uniqueThrice.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+bool getBit(int n, int pos){
+    return (n & (1<<pos))!=0;
+}
+int setBit(int n, int pos){
+    return (n | (1<<pos));
+}
+int unique(int a[], int n){
+    int result = 0;
+    for(int i=0; i<64; i++){
+        int sum = 0;
+        for(int j=0; j<n; j++)
+            if(getBit(a[j], i))
+                sum++;
+        if(sum%3!=0){
+            result = setBit(result, i);
+        }
+    }
+    return result;
+}
+int main(){
+    int a[] = {1, 2, 1, 4, 1, 2, 2};
+    cout<<unique(a, 7);
+}
